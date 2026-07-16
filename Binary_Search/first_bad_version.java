90fe77fd3478ab46d7b2470dc016ec29c9d047a3
Avika Goel <avikagoel15@gmail.com>
2026-07-16 11:50:19 +0530

    Binary search

---
+++ Binary_Search/first_bad_version.java
@@ -0,0 +1,28 @@
+public class first_bad_version {
+    /* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+    public static void main(String[] args) {
+        
+    }
+    public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        return firstbad(n);
+    }
+    public int firstbad(int n){
+           int lo=1;
+           int hi=n;
+           int ans=0;
+           while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if(isBadVersion(mid)==true){
+                ans=mid;
+                hi=mid-1;
+            }
+            else{
+                lo=mid+1;
+            }
+           }
+           return ans;
+    }
+}
+}
