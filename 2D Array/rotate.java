86453c209255c6607e7fb2eaaad6dbb360a9bb1f
Avika Goel <avikagoel1508@gmail.com>
2026-08-09 23:09:48 +0530

    questions

--- 2D Array/rotate.java
+++ 2D Array/rotate.java
@@ -10,10 +10,60 @@ public class rotate {
                 arr[i][j]=sc.nextInt();
             }
         }
-        rotate(arr);
+        rotate(arr, n);
     }
 
-    public static void rotate(int[][] arr) {
-        
+    public static void rotate(int[][] arr, int n) {
+        int[][] crr=new int[n][n];
+        int t=n-1;
+        for(int i=0; i<n && t>=0; i++){
+            for(int j=0; j<n && t>=0; j++){
+                crr[i][j]=arr[j][t];
+            }
+             t--;
+        }
+          for (int i = 0; i < crr.length; i++) {
+            for (int j = 0; j < crr[0].length; j++) {
+                System.out.print(crr[i][j]+" ");
+            }
+            System.out.println();
+          }
+    }
+    // one way is to use another array 
+
+
+// second way is to transpose the matrix adn then reverse it => this helps us to change the same matrix
+     public static void rotate(int[][] matrix) {
+
+        transpose(matrix);
+        for (int j = 0; j < matrix[0].length; j++) {
+            for (int i = 0; i < matrix.length / 2; i++) {
+
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[matrix.length - i - 1][j];
+                matrix[matrix.length - i - 1][j] = temp;
+            }
+        }
+             display(matrix);
+    }
+
+    public static void transpose(int[][] matrix) {
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[0].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+    public static void display(int[][] matrix) {
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
