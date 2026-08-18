aac475709063323c334462c1086df6d7cbbec798
Avika Goel <avikagoel1508@gmail.com>
2026-08-19 00:16:04 +0530

    Oops-1

---
+++ Oops-1/student.java
@@ -0,0 +1,8 @@
+public class student{
+    String name;
+    int age;
+
+    public void introduce(){
+        System.out.println("My name is " +name+ " my age is "+age);
+    }
+}
---
+++ Oops-1/student_client.java
@@ -0,0 +1,6 @@
+public class student_client {
+    public static void main(String[] args) {
+        // student.java is in same package so no need to import it
+        student s=new student();
+    }
+}
