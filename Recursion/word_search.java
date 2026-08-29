037982d8d3abd4af9f8b6a3c076b39ef24b8cb68
Avika Goel <avikagoel1508@gmail.com>
2026-08-29 17:04:15 +0530

    R&B-3

--- Recursion/word_search.java
+++ Recursion/word_search.java
@@ -1,11 +1,11 @@
 public class word_search {
     public static void main(String[] args) {
         char[][] board = { { 'A', 'B', 'C', 'E' }, { 'S', 'F', 'C', 'S' }, { 'A', 'D', 'E', 'E' } };
-		String word = "ABCCED";
+		String word = "ABCZ";
         for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board[0].length; j++) {
 				if (board[i][j] == word.charAt(0)) {
-					boolean ans = search(board, word, 0, 0, 0);
+					boolean ans = search(board, word, i, j, 0);
 					if (ans == true) {
 						System.out.println(ans);
 						return;
